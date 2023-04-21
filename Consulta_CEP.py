1335388dd2f3a6e6bb5aa94bc133a7a1b54e59a3
Gustavo Finau Pellanda <129123498+GustavoPellanda@users.noreply.github.com>
2023-04-21 20:04:24 -0300

    interface visual adicionada

--- Consulta_CEP.py
+++ Consulta_CEP.py
@@ -1,5 +1,6 @@
 import urllib.request
 import json
+import tkinter as tk
 
 def consulta_CEP(CEP):
     url = f"https://viacep.com.br/ws/{CEP}/json/"
@@ -7,31 +8,52 @@ def consulta_CEP(CEP):
         with urllib.request.urlopen(url) as buscaCEP:
             resultadoBusca = json.loads(buscaCEP.read().decode('utf-8'))
             if 'erro' in resultadoBusca and resultadoBusca['erro'] is True:
-                print("CEP não encontrado no banco de dados\n")
+                return "\nCEP não encontrado no banco de dados\n"
             else:
                 return resultadoBusca
     except urllib.error.HTTPError as codigoErro:
         if codigoErro.code == 400:
-            print("Formato de CEP inválido\n")
+            return "\nFormato de CEP inválido\n"
         else: 
-            print("Erro de comunicação\n")
+            return "\nErro de comunicação\n"
 
+def exibe_busca(CEP, caixa_pesquisa):
+    busca = consulta_CEP(CEP)
+    if isinstance(busca, dict):
+        resultado = f"Logradouro: {busca['logradouro']}\n" \
+                    f"Complemento: {busca['complemento']}\n" \
+                    f"Bairro: {busca['bairro']}\n" \
+                    f"Localidade: {busca['localidade']}\n" \
+                    f"UF: {busca['uf']}\n" \
+                    f"IBGE: {busca['ibge']}\n" \
+                    f"GIA: {busca['gia']}\n" \
+                    f"DDD: {busca['ddd']}\n" \
+                    f"SIAFI: {busca['siafi']}\n"
+    else:
+        resultado = busca
 
-CEP = input("\nCEP: ")
-busca = consulta_CEP(CEP)
-if busca:
-    print("Logradouro:", busca["logradouro"])
-    print("Complemento:", busca["complemento"])
-    print("Bairro:", busca["bairro"])
-    print("Localidade:", busca["localidade"])
-    print("UF:", busca["uf"])
-    print("IBGE:", busca["ibge"])
-    print("GIA:", busca["gia"])
-    print("DDD:", busca["ddd"])
-    print("SIAFI:", busca["siafi"],"\n")
+    caixa_pesquisa.insert(tk.END, resultado)
+    caixa_pesquisa.see(tk.END)
 
-# Aprendizados:
-# Usar with ao operar a url e para sair após erro
-# Usar decodificador
-# Usar urllib.error.HTTPError no except para códigos de erro
-# Usar is True para verificar se existe no BD
+def interface_usuario():
+    root = tk.Tk()
+    root.title("Consulte seu CEP!")
+
+    titulo = tk.Label(root, text="Consulte seu CEP!")
+    titulo.pack(pady=10)
+
+    entrada_CEP = tk.StringVar()
+    campo_entrada = tk.Entry(root, textvariable=entrada_CEP, width=50)
+    campo_entrada.pack(pady=10, ipadx=20)
+
+    caixa_pesquisa = tk.Text(root, height=10, width=50)
+    caixa_pesquisa.pack(pady=10)
+
+    botao_pesquisa = tk.Button(root, text="Pesquisar", command=lambda: exibe_busca(entrada_CEP.get(), caixa_pesquisa))
+    root.bind("<Return>", lambda event: exibe_busca(entrada_CEP.get(), caixa_pesquisa))
+    botao_pesquisa.pack(pady=10)
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    interface_usuario()
